a6b1f2ee7e5ad7b1de4df81449f56a04c7115bc6
KaitlynKeil <kaitly.keil@students.olin.edu>
2017-12-05 11:35:17 -0500

    node test added

--- oneDimensionalNode.v
+++ oneDimensionalNode.v
@@ -1,9 +1,8 @@
 `include "master_spi.v"
-`include "receiver_spi.v"
 `include "receiver_queue.v"
 `include "node_controller.v"
 
-module oneDimensionalNode(shiftInLeftData,shiftInLeftCS,shiftInRightData,shiftInRightCS,shiftInData,shiftInCLK,shiftInCS,shiftOutLeftCS,shiftOutRightCS,shiftOutData,shiftOutCLK,shiftOutCS);
+module oneDimensionalNode(shiftInLeftData,shiftInLeftCS,shiftInRightData,shiftInRightCS,shiftInData,shiftInCLK,shiftInCS,shiftOutLeftCS,shiftOutRightCS,shiftOutLeftData,shiftOutRightData,shiftOutData,shiftOutCLK,shiftOutCS);
 
 	input [31:0] shiftInLeftData,shiftInRightData,shiftInData;
 	input shiftInLeftCS,shiftInRightCS,shiftInCS,shiftInCLK;
@@ -13,12 +12,12 @@ module oneDimensionalNode(shiftInLeftData,shiftInLeftCS,shiftInRightData,shiftIn
 	wire [31:0] fromRightData,fromLeftData,fromData,instruction,instructionTo;
 	wire fromRightCS,fromLeftCS,fromCS,controllerEn;
 	wire [1:0] dataSource,outputSelect;
-	
-	receiver_queue receiverQueue(.clk (clk),.check_l (shiftInLeftCS),.check_r (shiftInRightCS),.check_s (shiftInCS),.in_sig_right (shiftInRightData),.in_sig_left (shiftInLeftData),.in_sig_self (shiftInData),.selected_sig (instruction),.sig_alert (controllerEn),.s (dataSource));
-	
-	node_controller nodeController(.clk (clk),.source_port (dataSource),.controller_enable (controllerEn),.instruction_in (instruction),.instruction_out (instructionTo),.enable (outputSelect));
 
-	master_spi masterSPI(.clk (clk),.enable (outputSelect),.in_instr(instructionTo),.check_self(shiftOutCS),.check_left(shiftOutleftCS),.check_right(shiftOutRightCS),.out_instr(shiftOutData),.clk_out(shiftOutCLK));
+	receiver_queue receiverQueue(.clk (shiftInCLK),.check_l (shiftInLeftCS),.check_r (shiftInRightCS),.check_s (shiftInCS),.in_sig_right (shiftInRightData),.in_sig_left (shiftInLeftData),.in_sig_self (shiftInData),.selected_sig (instruction),.sig_alert (controllerEn),.s (dataSource));
+	
+	node_controller nodeController(.clk (shiftInCLK),.source_port (dataSource),.controller_enable (controllerEn),.instruction_in (instruction),.instruction_out (instructionTo),.enable (outputSelect));
+
+	master_spi masterSPI(.clk (shiftInCLK),.enable (outputSelect),.in_instr(instructionTo),.check_self(shiftOutCS),.check_left(shiftOutleftCS),.check_right(shiftOutRightCS),.out_instr(shiftOutData),.clk_out(shiftOutCLK));
 	
 endmodule
 	
