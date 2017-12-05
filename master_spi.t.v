299319cbe10cd758e5935f4f02d42cc8909ca8dd
Prava Dhulipalla <prava@students.olin.edu>
2017-12-04 21:53:52 -0500

    tested, works as I implemented

--- master_spi.t.v
+++ master_spi.t.v
@@ -2,18 +2,20 @@
 
 module master_spi_test();
 
-	reg clk, new_sig;
+	reg clk;
+	reg [1:0] enable;
 	reg [31:0] in_instr;
-	reg [7:0] address;
 
-	wire check;
+	wire check_self, check_left, check_right;
 	wire [31:0] out_instr;
 
-	master_spimemory dut(.clk(clk),
-		.assert(new_sig),
+
+	master_spi dut(.clk(clk),
+		.enable(enable),
 		.in_instr(in_instr),
-		.address(address),
-		.check(check),
+		.check_self(check_self),
+		.check_left(check_left),
+		.check_right(check_right),
 		.out_instr(out_instr));
 
 
@@ -29,7 +31,7 @@ module master_spi_test();
 
 		begin
 			if ((instr_check != exp_check) || (out_instr != exp_instr)) begin
-				$display("Test failed!");
+				$display("SPI Test failed!");
 				$display("Check was %b and was expected to be %b.", instr_check, exp_check);
 				$display("Instr was %b and was expected to be %b.", out_instr, exp_instr);
 			end
@@ -40,19 +42,11 @@ module master_spi_test();
 		$dumpfile("master_spi.vcd");
 		$dumpvars();
 
-		address <= 8'b0; #100
+		enable <=  2'b0; in_instr <= 32'd30000; #100
 
-		new_sig <= 1'b0; in_instr <= 32'd50000; #100
-		checkTestCase(check, new_sig, out_instr, 32'bx); #50
+		enable <= 2'b1; in_instr <= 32'd50000; #100
 
-		new_sig <= 1'b1; in_instr <= 32'd50000; #100
-		checkTestCase(check, new_sig, out_instr, in_instr); #50
-
-		new_sig <= 1'b0; in_instr <= 32'd10000; #100
-		checkTestCase(check, new_sig, out_instr, 32'd50000); #50
-
-		new_sig <= 1'b1; in_instr <= 32'd10000; #100
-		checkTestCase(check, new_sig, out_instr, in_instr); #50
+		enable <= 2'b10; in_instr <= 32'd10000; #100
 
 		$finish();
 	end
