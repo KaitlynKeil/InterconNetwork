fc91723af7758f46d0da773eda1f93d200b9eec4
Bryan Werth <bryan.werth@students.olin.edu>
2017-12-08 12:22:09 -0500

    Started adding connections between the nodes in the multi_node_test bench

--- multi_node_test.t.v
+++ multi_node_test.t.v
@@ -3,23 +3,23 @@
 module multi_node_test ();
 	// Test containing 5 nodes
 	// NOTE: These are the only important inputs/outputs. I think it should be enough to include only them? Not sure why oneDimensionalNode has all the rest as outputs
-	oneDimensionalNode #(.NODE_IP (3'b000),.MIDPOINT_NODE(3'b010)) node000(.shiftInCLK,
-																		.shiftInLeftData(), .shiftInRightData(), .shiftInData(),
-																		.shiftInLeftCS(), .shiftInRightCS(), .shiftInCs(),
-																		.shiftOutLeftData(), .shiftOutRightData(), .shiftOutData(),
-																		.shiftOutLeftCS(), .shiftOutRightCS(), .shiftOutCS());
-	oneDimensionalNode #(.NODE_IP (3'b001),.MIDPOINT_NODE(3'b010)) node001(.shiftInCLK,
-																		.shiftInLeftData(), .shiftInRightData(), .shiftInData(),
-																		.shiftInLeftCS(), .shiftInRightCS(), .shiftInCs(),
-																		.shiftOutLeftData(), .shiftOutRightData(), .shiftOutData(),
-																		.shiftOutLeftCS(), .shiftOutRightCS(), .shiftOutCS());
-	oneDimensionalNode #(.NODE_IP (3'b010),.MIDPOINT_NODE(3'b010)) node010(.shiftInCLK,
-																		.shiftInLeftData(), .shiftInRightData(), .shiftInData(),
-																		.shiftInLeftCS(), .shiftInRightCS(), .shiftInCs(),
-																		.shiftOutLeftData(), .shiftOutRightData(), .shiftOutData(),
-																		.shiftOutLeftCS(), .shiftOutRightCS(), .shiftOutCS());
-	oneDimensionalNode #(.NODE_IP (3'b011),.MIDPOINT_NODE(3'b010)) node011(.shiftInCLK,
-																		.shiftInLeftData(), .shiftInRightData(), .shiftInData(),
+	oneDimensionalNode #(.NODE_IP (3'b000),.MIDPOINT_NODE(3'b010)) node000(.shiftInCLK(clk),
+																		.shiftInLeftData(oneToZeroData), .shiftInRightData(fourToZeroData), .shiftInData(zeroInData),
+																		.shiftInLeftCS(oneToZeroCS), .shiftInRightCS(fourToZeroCS), .shiftInCs(zeroInCS),
+																		.shiftOutLeftData(zeroToFourData), .shiftOutRightData(zeroToOneData), .shiftOutData(zeroOutData),
+																		.shiftOutLeftCS(zeroToFourCS), .shiftOutRightCS(zeroToOneCS), .shiftOutCS(zeroOutCS));
+	oneDimensionalNode #(.NODE_IP (3'b001),.MIDPOINT_NODE(3'b010)) node001(.shiftInCLK(clk),
+																		.shiftInLeftData(twoToOneData), .shiftInRightData(zeroToOneData), .shiftInData(oneInData),
+																		.shiftInLeftCS(twoToOneCS), .shiftInRightCS(zeroToOneCS), .shiftInCs(oneInCS),
+																		.shiftOutLeftData(oneToZeroData), .shiftOutRightData(oneToTwoData), .shiftOutData(oneOutData),
+																		.shiftOutLeftCS(oneToZeroCS), .shiftOutRightCS(oneToTwoCS), .shiftOutCS(oneOutCS));
+	oneDimensionalNode #(.NODE_IP (3'b010),.MIDPOINT_NODE(3'b010)) node010(.shiftInCLK(clk),
+																		.shiftInLeftData(threeToTwoData), .shiftInRightData(oneToTwoData), .shiftInData(twoInData),
+																		.shiftInLeftCS(threeToTwoCS), .shiftInRightCS(oneToTwoCS), .shiftInCs(twoInCS),
+																		.shiftOutLeftData(twoToOneData), .shiftOutRightData(twoToThreeData), .shiftOutData(twoOutData),
+																		.shiftOutLeftCS(twoToOneCS), .shiftOutRightCS(twoToThreeCS), .shiftOutCS(twoOutCS));
+	oneDimensionalNode #(.NODE_IP (3'b011),.MIDPOINT_NODE(3'b010)) node011(.shiftInCLK(clk),
+																		.shiftInLeftData(fourToThreeData), .shiftInRightData(), .shiftInData(),
 																		.shiftInLeftCS(), .shiftInRightCS(), .shiftInCs(),
 																		.shiftOutLeftData(), .shiftOutRightData(), .shiftOutData(),
 																		.shiftOutLeftCS(), .shiftOutRightCS(), .shiftOutCS());
