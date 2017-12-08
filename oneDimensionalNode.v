06e7f9c1ea20c2c5e433af24ca2412575cdb7b0e
KaitlynKeil <kaitly.keil@students.olin.edu>
2017-12-08 12:20:38 -0500

    Adding comments, cleaning up

--- oneDimensionalNode.v
+++ oneDimensionalNode.v
@@ -2,24 +2,20 @@
 `include "receiver_queue.v"
 `include "node_controller.v"
 
-module oneDimensionalNode(shiftInLeftData,shiftInLeftCS,shiftInRightData,shiftInRightCS,shiftInData,shiftInCLK,shiftInCS,shiftOutLeftCS,shiftOutRightCS,shiftOutLeftData,shiftOutRightData,shiftOutData,shiftOutCLK,shiftOutCS,dataSource,outputSelect,controllerEn,instruction,instructionTo,controller_enable_out);
+module oneDimensionalNode(shiftInLeftData,shiftInLeftCS,shiftInRightData,shiftInRightCS,shiftInData,shiftInCLK,shiftInCS,shiftOutLeftCS,shiftOutRightCS,shiftOutLeftData,shiftOutRightData,shiftOutData,shiftOutCS);
 
 	input [31:0] shiftInLeftData,shiftInRightData,shiftInData;
 	input shiftInLeftCS,shiftInRightCS,shiftInCS,shiftInCLK;
 	output [31:0] shiftOutLeftData,shiftOutRightData,shiftOutData;
-	output shiftOutLeftCS,shiftOutRightCS,shiftOutCS,shiftOutCLK;
-	output [1:0] dataSource,outputSelect;
-	output controllerEn;
-	output [31:0] instruction,instructionTo;
-	output controller_enable_out;
+	output shiftOutLeftCS,shiftOutRightCS,shiftOutCS;
 	
 	parameter NODE_IP = 3'b000;
 	parameter MIDPOINT_NODE = 3'b011;
 	parameter NODE_IP_BITWIDTH = 3;
 	parameter width = 32;
 	
-	wire [31:0] fromRightData,fromLeftData,fromData,instruction,instructionTo;
-	wire fromRightCS,fromLeftCS,fromCS,controllerEn;
+	wire [31:0] instruction,instructionTo;
+	wire controller_enable_out, controllerEn;
 	wire [1:0] dataSource,outputSelect;
 
 	receiver_queue #(.width(32)) receiverQueue(.clk (shiftInCLK),.check_l (shiftInLeftCS),.check_r (shiftInRightCS),.check_s (shiftInCS),.in_sig_right (shiftInRightData),.in_sig_left (shiftInLeftData),.in_sig_self (shiftInData),.selected_sig (instruction),.sig_alert (controllerEn),.s (dataSource));
